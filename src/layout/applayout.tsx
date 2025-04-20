bb079c5833fe1e23255d5a03b2183fa278e6c31e
Jane Shin <janeshin@janeuimcbookair.wifi.local.cmu.edu>
2025-04-20 17:48:19 -0400

    Design0420

--- src/layout/applayout.tsx
+++ src/layout/applayout.tsx
@@ -8,6 +8,17 @@ import {
   QuestionCircleOutlined,
   MenuFoldOutlined,
   MenuUnfoldOutlined,
+  ClockCircleOutlined,
+  HighlightOutlined,
+  EditOutlined,
+  TableOutlined,
+  DatabaseOutlined,
+  SafetyOutlined,
+  WarningOutlined,
+  SendOutlined,
+  DashboardOutlined,
+  FormOutlined,
+  CheckCircleOutlined
 } from '@ant-design/icons';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
@@ -23,6 +34,7 @@ interface AppLayoutProps {
   pageTitles: Record<string, string>;
 }
 
+//컴포넌트 본문
 const AppLayout: React.FC<AppLayoutProps> = ({
   collapsed,
   setCollapsed,
@@ -31,17 +43,20 @@ const AppLayout: React.FC<AppLayoutProps> = ({
   menuGroups,
   pageTitles,
 }) => {
-  const location = useLocation();
-  const navigate = useNavigate();
+  const location = useLocation(); // 현재 경로 추적
+  const navigate = useNavigate(); // 버튼 클릭 등 페이지 이동
 
+  {/*경로에서 메뉴 키 추출*/}
   const path = location.pathname.slice(1);
   const segments = path.split('/');
   const key = segments.length === 1 ? segments[0] : `${segments[0]}/${segments[1]}`;
 
+  {/*경로 바뀔 때마다 selectMenu 값을 동기화*/}
   useEffect(() => {
     setSelectedMenu(key || 'dashboard');
   }, [location.pathname]);
 
+  {/*헤더, 사이드바, 메뉴, 브레드크럼 */}
   return (
     <Layout style={{ minHeight: '100vh', paddingTop: 56 }}>
       <Header
@@ -67,7 +82,9 @@ const AppLayout: React.FC<AppLayoutProps> = ({
               fillRule="evenodd"
               clipRule="evenodd"
               d="M13.5492 0.112436C13.8089 -0.0374786 14.1288 -0.0374786 14.3885 0.112436L27.5181 7.69282C27.7777 7.84273 27.9377 8.11979 27.9377 8.41962V23.5804C27.9377 23.8802 27.7777 24.1573 27.5181 24.3072L14.3885 31.8876C14.1288 32.0375 13.8089 32.0375 13.5492 31.8876L0.419631 24.3072C0.159972 24.1573 0 23.8802 0 23.5804V8.41962C0 8.11979 0.159972 7.84273 0.419631 7.69282L13.5492 0.112436ZM25.42 8.41961L13.9689 15.0309L2.51771 8.41961L13.9689 1.80829L25.42 8.41961Z"
-              fill="#93B04C"
+              fill="none"
+              stroke="#515A48"
+              stroke-width = "2"
             />
           </svg>
           <Text style={{ color: 'white', fontWeight: 700, fontSize: 16 }}>CARGOOPS</Text>
@@ -100,16 +117,6 @@ const AppLayout: React.FC<AppLayoutProps> = ({
               gap: 8,
             }}
           >
-            <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="28" height="32" viewBox="0 0 28 32" fill="none">
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M13.5492 0.112436C13.8089 -0.0374786 14.1288 -0.0374786 14.3885 0.112436L27.5181 7.69282C27.7777 7.84273 27.9377 8.11979 27.9377 8.41962V23.5804C27.9377 23.8802 27.7777 24.1573 27.5181 24.3072L14.3885 31.8876C14.1288 32.0375 13.8089 32.0375 13.5492 31.8876L0.419631 24.3072C0.159972 24.1573 0 23.8802 0 23.5804V8.41962C0 8.11979 0.159972 7.84273 0.419631 7.69282L13.5492 0.112436ZM25.42 8.41961L13.9689 15.0309L2.51771 8.41961L13.9689 1.80829L25.42 8.41961Z"
-                  fill="#93B04C"
-                />
-              </svg>
-            </div>
             <div onClick={() => setCollapsed(!collapsed)} style={{ cursor: 'pointer' }}>
               {collapsed ? <MenuUnfoldOutlined style={{ fontSize: 18 }} /> : <MenuFoldOutlined style={{ fontSize: 18 }} />}
             </div>
@@ -123,32 +130,32 @@ const AppLayout: React.FC<AppLayoutProps> = ({
               navigate(`/${e.key}`);
             }}
           >
-            <Menu.Item key="dashboard">Dashboard</Menu.Item>
-            <Menu.Item key="master">Master</Menu.Item>
+            <Menu.Item key="dashboard" icon={<DashboardOutlined />}>Dashboard</Menu.Item>
+            <Menu.Item key="master" icon={<HighlightOutlined />}>Master</Menu.Item>
 
-            <Menu.SubMenu key="receiving" title="Receiving">
+            <Menu.SubMenu key="receiving" icon={<FormOutlined />} title="Receiving">
               <Menu.Item key="receiving/storing">Storing Order Request</Menu.Item>
               <Menu.Item key="receiving/myreceiving">My Receiving List</Menu.Item>
             </Menu.SubMenu>
 
-            <Menu.Item key="qc">Quality Check</Menu.Item>
+            <Menu.Item key="qc" icon={<TableOutlined />}>Quality Check</Menu.Item>
 
-            <Menu.SubMenu key="binning" title="Binning">
+            <Menu.SubMenu key="binning" icon={<DatabaseOutlined />} title="Binning">
               <Menu.Item key="binning/assign">Bin Assignment</Menu.Item>
               <Menu.Item key="binning/my">My Binning</Menu.Item>
             </Menu.SubMenu>
 
-            <Menu.SubMenu key="inventory" title="Inventory">
+            <Menu.SubMenu key="inventory" icon={<CheckCircleOutlined />} title="Inventory">
               <Menu.Item key="inventory/management">Inventory Management</Menu.Item>
               <Menu.Item key="inventory/reconciliation">Inventory Reconciliation</Menu.Item>
             </Menu.SubMenu>
 
-            <Menu.SubMenu key="picking" title="Picking">
+            <Menu.SubMenu key="picking" icon={<WarningOutlined />} title="Picking">
               <Menu.Item key="picking/mypicking">My Picking</Menu.Item>
               <Menu.Item key="picking/pickslip">Pick Slip</Menu.Item>
             </Menu.SubMenu>
 
-            <Menu.SubMenu key="dispatch" title="Dispatch">
+            <Menu.SubMenu key="dispatch" icon={<SendOutlined />} title="Dispatch">
               <Menu.Item key="dispatch/mypacking">My Packing</Menu.Item>
               <Menu.Item key="dispatch/inspection">Dispatch Inspection</Menu.Item>
             </Menu.SubMenu>
