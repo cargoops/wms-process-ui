00912bbee2ca5f2314ff1b2543c756e28c9bba6c
Jane Shin <janeshin@janeuimcbookair.wifi.local.cmu.edu>
2025-05-08 14:47:22 -0400

    SVGs

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -31,98 +31,140 @@ export default function LoginPage({ onLogin }: { onLogin: () => void }) {
   };
 
   return (
-    <div
-      style={{
-        minHeight: '100vh',
-        background: 'var(--Conditional-page-background, #F0F2F5)',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-      }}
-    >
-      {/* 로고 */}
-      <div style={{ textAlign: 'center', marginBottom: 24 }}>
-        <img
-          src="/logo192.png" // 원하는 로고 이미지 경로로 교체
-          alt="logo"
-          style={{ width: 40, marginBottom: 12 }}
-        />
-        <Title level={3} style={{ margin: 0 }}>
-          CargoOps
-        </Title>
-        <Text type="secondary">End-to-End Warehouse Management System</Text>
-      </div>
+    <div style={{ position: 'relative', minHeight: '100vh', overflow: 'hidden' }}>
+      {/* 배경 SVG들 */}
+      <img
+        src="/Group14_login.svg"
+        alt="top-left-decoration"
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: 200,
+          zIndex: 0,
+        }}
+      />
+      <img
+        src="/Group18_login.svg"
+        alt="bottom-left-decoration"
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          width: 200,
+          zIndex: 0,
+        }}
+      />
+      <img
+        src="/Group10_login.svg"
+        alt="top-right-decoration"
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          width: 200,
+          zIndex: 0,
+        }}
+      />
 
-      {/* 로그인 카드 */}
-      <Card style={{ width: 300 }}>
-        <Title level={5} style={{ marginBottom: 24 }}>
-          Login
-        </Title>
+      {/* 로그인 콘텐츠 */}
+      <div
+        style={{
+          position: 'relative',
+          zIndex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          minHeight: '100vh',
+          background: 'var(--Conditional-page-background, #F0F2F5)'
+        }}
+      >
+        {/* 로고 */}
+        <div style={{ textAlign: 'center', marginBottom: 24 }}>
+          <img
+            src="/logo192.png"
+            alt="logo"
+            style={{ width: 40, marginBottom: 12 }}
+          />
+          <Title level={3} style={{ margin: 0 }}>
+            CargoOps
+          </Title>
+          <Text type="secondary">End-to-End Warehouse Management System</Text>
+        </div>
 
-        <Input
-          size="large"
-          placeholder="Scan ID or Type Username"
-          prefix={<UserOutlined />}
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          style={{ marginBottom: 16 }}
-        />
-        <Input.Password
-          size="large"
-          placeholder="password: ant.design"
-          prefix={<LockOutlined />}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{ marginBottom: 16 }}
-        />
+        {/* 로그인 카드 */}
+        <Card style={{ width: 300 }}>
+          <Title level={5} style={{ marginBottom: 24 }}>
+            Login
+          </Title>
 
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginBottom: 16
-          }}
-        >
-          <Checkbox
-            checked={remember}
-            onChange={(e) => setRemember(e.target.checked)}
+          <Input
+            size="large"
+            placeholder="Scan ID or Type Username"
+            prefix={<UserOutlined />}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            style={{ marginBottom: 16 }}
+          />
+          <Input.Password
+            size="large"
+            placeholder="password: ant.design"
+            prefix={<LockOutlined />}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ marginBottom: 16 }}
+          />
+
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginBottom: 16
+            }}
           >
-            Remember me
-          </Checkbox>
-          <Link>Forgot your</Link>
-        </div>
+            <Checkbox
+              checked={remember}
+              onChange={(e) => setRemember(e.target.checked)}
+            >
+              Remember me
+            </Checkbox>
+            <Link>Forgot your</Link>
+          </div>
 
-        <Button
-          type="primary"
-          block
-          size="large"
-          onClick={handleLogin}
-          style={{ marginBottom: 16 }}
-        >
-          Sign In
-        </Button>
+          <Button
+            type="primary"
+            block
+            size="large"
+            onClick={handleLogin}
+            style={{ marginBottom: 16 }}
+          >
+            Sign In
+          </Button>
 
-        <Divider style={{ margin: '12px 0' }}>Quick Sign-in:</Divider>
+          <Divider style={{ margin: '12px 0' }}>Quick Sign-in:</Divider>
 
-        <div style={{ textAlign: 'center' }}>
-          <Space size="large">
-            <AlipayCircleOutlined style={{ fontSize: 20 }} />
-            <TaobaoCircleOutlined style={{ fontSize: 20 }} />
-            <WeiboCircleOutlined style={{ fontSize: 20 }} />
-          </Space>
-        </div>
+          <div style={{ textAlign: 'center' }}>
+            <Space size="large">
+              <AlipayCircleOutlined style={{ fontSize: 20 }} />
+              <TaobaoCircleOutlined style={{ fontSize: 20 }} />
+              <WeiboCircleOutlined style={{ fontSize: 20 }} />
+            </Space>
+          </div>
 
-        <div style={{ textAlign: 'center', marginTop: 24 }}>
-          <Link>Sign Up</Link>
-        </div>
-      </Card>
+          <div style={{ textAlign: 'center', marginTop: 24 }}>
+            <Link>Sign Up</Link>
+          </div>
+        </Card>
 
-      {/* 하단 푸터 */}
-      <div style={{ marginTop: 32, textAlign: 'center', color: '#999' }}>
-        <div style={{ marginBottom: 4 }}>Ant Design Pro &nbsp; | &nbsp; Ant Design</div>
-        <div style={{ fontSize: 12 }}>
-          Copyright ©2020 Produced by Ant Finance Experience Technology Department
+        {/* 하단 푸터 */}
+        <div style={{ marginTop: 32, textAlign: 'center', color: '#999' }}>
+          <div style={{ marginBottom: 4 }}>
+            Ant Design Pro &nbsp; | &nbsp; Ant Design
+          </div>
+          <div style={{ fontSize: 12 }}>
+            Copyright ©2020 Produced by Ant Finance Experience Technology Department
+          </div>
         </div>
       </div>
     </div>
